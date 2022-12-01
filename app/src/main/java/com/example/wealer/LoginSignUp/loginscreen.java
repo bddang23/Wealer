9a4cbb038b4bbe74b761a37853b3ce56aac0ddbb
Bean <bddang@svsu.edu>
2022-11-30 23:32:42 -0500

    Adding Activity Without Designing

--- app/src/main/java/com/example/wealer/LoginSignUp/loginscreen.java
+++ app/src/main/java/com/example/wealer/LoginSignUp/loginscreen.java
@@ -1,14 +1,16 @@
-package com.example.wealer;
+package com.example.wealer.LoginSignUp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.wealer.R;
+
+public class loginscreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login_screen_activity);
     }
 }
---
+++ app/src/main/java/com/example/wealer/userListing/manageListing.java
@@ -0,0 +1,39 @@
+package com.example.wealer.userListing;
+
+import android.os.Bundle;
+
+import com.example.wealer.R;
+import com.google.android.material.snackbar.Snackbar;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import com.example.wealer.databinding.ManageListingActivityBinding;
+
+public class manageListing extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+private ManageListingActivityBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+     binding = ManageListingActivityBinding.inflate(getLayoutInflater());
+     setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.toolbar);
+
+        binding.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+
+}
