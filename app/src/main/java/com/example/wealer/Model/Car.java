83090f3ee031986d210f31759decaad330fd3894
Bean <bddang@svsu.edu>
2022-12-04 18:50:07 -0500

    Set Up and Design Some  Activity

--- app/src/main/java/com/example/wealer/Model/Car.java
+++ app/src/main/java/com/example/wealer/Model/Car.java
@@ -1,16 +1,18 @@
 package com.example.wealer.Model;
 
 public class Car {
-   private String make;
-   private String model;
-   private String description;
-   private String imageLink;
-   private float price;
-   private String address;
+    private String make;
+    private String model;
+    private int miles;
+    private String description;
+    private String imageLink;
+    private float price;
+    private String address;
 
-    public Car(String make, String model, String description, String imageLink, float price, String address) {
+    public Car(String make, String model, int miles, String description, String imageLink, float price, String address) {
         this.make = make;
         this.model = model;
+        this.miles = miles;
         this.description = description;
         this.imageLink = imageLink;
         this.price = price;
@@ -31,6 +33,14 @@ public class Car {
 
     public void setModel(String model) {
         this.model = model;
+    }
+
+    public int getMiles() {
+        return miles;
+    }
+
+    public void setMiles(int miles) {
+        this.miles = miles;
     }
 
     public String getDescription() {
--- app/src/main/java/com/example/wealer/userListing/manageListing.java
+++ app/src/main/java/com/example/wealer/userListing/manageListing.java
@@ -2,29 +2,37 @@ package com.example.wealer.userListing;
 
 import android.os.Bundle;
 
-import com.example.wealer.R;
+import com.example.wealer.databinding.ActivityManageListingBinding;
 import com.google.android.material.snackbar.Snackbar;
+
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.view.View;
+
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-import com.example.wealer.databinding.ManageListingActivityBinding;
+
+
+import com.example.wealer.R;
 
 public class manageListing extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
-private ManageListingActivityBinding binding;
+    private ActivityManageListingBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-     binding = ManageListingActivityBinding.inflate(getLayoutInflater());
-     setContentView(binding.getRoot());
+        binding = ActivityManageListingBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        setSupportActionBar(binding.toolbar);
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_manage_listing);
+        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
 
         binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,5 +43,10 @@ private ManageListingActivityBinding binding;
         });
     }
 
-
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_manage_listing);
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
 }
