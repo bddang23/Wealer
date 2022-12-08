b52d677aea7a70473d0be7ecd4ee8c39c7e3b500
Bailey Steele <bjsteel1@svsu.edu>
2022-12-07 19:00:51 -0500

    init commit

--- app/src/main/java/com/example/wealer/DashBoard/mapListing.java
+++ app/src/main/java/com/example/wealer/DashBoard/mapListing.java
@@ -1,16 +1,31 @@
 package com.example.wealer.DashBoard;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
 import com.example.wealer.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
 
-public class mapListing extends AppCompatActivity {
-
+public class mapListing extends AppCompatActivity implements OnMapReadyCallback {
+    GoogleMap map;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.map_listing_activity);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.mapView);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        map = googleMap;
+
     }
 }
